7b47fef04f95bdfda45433e8fd5ebe056749001c
kjg1993 <kguzman077@gmail.com>
2021-10-09 01:08:39 -0500

    updating my code of lesson 4

---
+++ lesson4/js/main.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", function () {
+	let sliderImgs = [];
+	sliderImgs[0] = "images/img1.jpg";
+	sliderImgs[1] = "images/img2.jpg";
+	sliderImgs[2] = "images/img3.jpg";
+	sliderImgs[3] = "images/img4.jpg";
+	sliderImgs[4] = "images/img5.jpg";
+	sliderImgs[5] = "images/img6.jpg";
+	sliderImgs[6] = "images/img7.jpg";
+	sliderImgs[7] = "images/img8.jpg";
+	sliderImgs[8] = "images/img9.jpg";
+
+	let indexImages = 0,
+		time = 1500;
+
+	function changeImages() {
+		document.slider.src = sliderImgs[indexImages];
+
+		if (indexImages < 8) {
+			indexImages++;
+		} else {
+			indexImages = 0;
+		}
+	}
+	this.setInterval(changeImages, time);
+});
+
+updateModified = function () {
+	let currentDate = new Date();
+	let infoDate = {year: "numeric", month: "long", day: "numeric"};
+	const showDate = currentDate.toLocaleDateString("en-Uk", infoDate);
+
+	document.getElementById("date").textContent = showDate;
+};
+
+updateModified();
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+	"click",
+	() => {
+		mainnav.classList.toggle("responsive");
+	},
+	false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+	if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
