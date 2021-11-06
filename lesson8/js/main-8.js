60ddb0743f8874bfe6501578ab8220277872ebc9
kjg1993 <kguzman077@gmail.com>
2021-11-06 16:51:30 -0500

    upload week eight activity

---
+++ lesson8/js/main-8.js
@@ -0,0 +1,40 @@
+const updateModified = function () {
+	let currentDate = new Date();
+
+	const infoDate = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	};
+
+	document.getElementById("date").textContent = currentDate.toLocaleDateString(
+		"en-Uk",
+		infoDate
+	);
+};
+updateModified();
+
+/// button resposive menu
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+	"click",
+	() => {
+		mainnav.classList.toggle("responsive");
+	},
+	false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+	if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+//----- Adjust the rating
+
+function adjustRating(rating) {
+	document.getElementById("ratingvalue").innerHTML = rating;
+}
