659d7977061d9672dd632a7e6e7e5eaae11d392d
kjg1993 <kguzman077@gmail.com>
2021-12-15 09:57:34 -0500

    Uploading changes

--- final_project/js/directory.js
+++ final_project/js/directory.js
@@ -1,62 +1,38 @@
 "use strict";
 /// Directory Api
 
-// const showDirectory = (items) => {
-// 	items.forEach((direcInfo) => {
-// 		let container = document.createElement("div");
-// 		container.className = "content-card";
+const urlApi =
+	"https://kjg1993.github.io/wdd-230/bussines_info_Json/bussinesInfo.json";
 
-// 		let name = document.createElement("h2");
-// 		name.className = "name";
-// 		name.textContent = direcInfo.name;
+fetch(urlApi)
+	.then((response) => response.json())
+	.then((directoryApi) => {
+		let info = directoryApi["businnesInfo"];
+		console.log(info);
 
-// 		let logo = document.createElement("img");
-// 		logo.localName = "logo";
-// 		logo.setAttribute("src", direcInfo.logo);
-// 		logo.setAttribute("alt", direcInfo.name);
+		for (let i = 0; i < info.length; i++) {
+			const element = info[i];
 
-// 		let web = document.createElement("a");
-// 		web.className = "web";
-// 		web.setAttribute("href", direcInfo.urlSite);
+			let container = document.createElement("div");
+			container.className = "content-card";
 
-// 		container.appendChild(name);
-// 		container.appendChild(logo);
-// 		container.appendChild(web);
-// 		document.querySelector(".directory").appendChild(container);
-// 	});
-// };
+			let name = document.createElement("h2");
+			name.className = "name";
+			name.textContent = element.name;
 
-// const urlApi =
-// 	"https://kjg1993.github.io/wdd-230/bussines_info_Json/bussinesInfo.json";
+			let logo = document.createElement("img");
+			logo.className = "logo";
+			logo.setAttribute("src", element.logo);
+			logo.setAttribute("alt", element.name);
 
-// fetch(urlApi)
-// 	.then((response) => response.json())
-// 	.then((directoryApi) => {
-// 		let info = directoryApi["businnesInfo"];
-// 		console.log(info);
+			let web = document.createElement("a");
+			web.textContent = element.name;
+			web.setAttribute("href", element.urlSite);
+			web.setAttribute("target", "_blank");
 
-// 		for (let i = 0; i < info.length; i++) {
-// 			const element = info[i];
-
-// 			let container = document.createElement("div");
-// 			container.className = "content-card";
-
-// 			let name = document.createElement("h2");
-// 			name.className = "name";
-// 			name.textContent = element.name;
-
-// 			let logo = document.createElement("img");
-// 			logo.className = "logo";
-// 			logo.setAttribute("src", element.logo);
-// 			logo.setAttribute("alt", element.name);
-
-// 			let web = document.createElement("a");
-// 			web.className = "web";
-// 			web.setAttribute("href", element.urlSite);
-
-// 			container.appendChild(name);
-// 			container.appendChild(logo);
-// 			container.appendChild(web);
-// 			document.querySelector(".directory").appendChild(container);
-// 		}
-// 	});
+			container.appendChild(name);
+			container.appendChild(logo);
+			container.appendChild(web);
+			document.querySelector(".directory").appendChild(container);
+		}
+	});
